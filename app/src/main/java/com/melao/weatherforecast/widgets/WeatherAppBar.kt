7afdc1af1f2435579ae3718c8745b787e94cd096
darinwilliams <darinjswilliams@icloud.com>
2023-03-18 21:58:00 -0500

    Added System Menu with icons

--- app/src/main/java/com/melao/weatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/melao/weatherforecast/widgets/WeatherAppBar.kt
@@ -1,18 +1,22 @@
 package com.melao.weatherforecast.widgets
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,6 +34,13 @@ fun WeatherAppBar(
     onButtonClicked: () -> Unit = {}
 
 ) {
+    val showDialog = remember {
+        mutableStateOf(value = false)
+    }
+
+    if (showDialog.value) {
+        ShowSettingDropDownMenu(showDialog = showDialog, navController = navController)
+    }
     TopAppBar(
         title = {
             Text(
@@ -45,7 +56,7 @@ fun WeatherAppBar(
                     Icon(imageVector = Icons.Default.Search, contentDescription = "Search Icon")
                 }
                 //Add more icon, which is the 3 elipse
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { showDialog.value = true }) {
                     Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "More Icon")
 
                 }
@@ -64,4 +75,50 @@ fun WeatherAppBar(
         elevation = elevation
     )
 
+}
+
+@Composable
+fun ShowSettingDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+
+    var expanded by remember {
+        mutableStateOf(true)
+    }
+
+    val items = listOf("About", "Favorites", "Settings")
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentSize(Alignment.TopEnd)
+            .absolutePadding(top = 45.dp, right = 20.dp)
+    ) {
+
+        //Dropdown Menu from Compose
+        DropdownMenu(
+            expanded = expanded, onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)
+        ) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    showDialog.value = false
+                }) {
+                    Icon(
+                        imageVector = when (text) {
+                            "About" -> Icons.Default.Info
+                            "Favorites" -> Icons.Default.FavoriteBorder
+                            else -> Icons.Default.Settings
+                        }, contentDescription = null,
+                        tint = Color.LightGray
+                    )
+                    Text(text = text, modifier = Modifier.clickable {
+
+                    }, fontWeight = FontWeight.W300)
+                }
+
+            }
+        }
+    }
 }
